ce82706b326016da97cd3243e014461ab977415e
azarft <argoazanov@gmail.com>
2024-03-11 22:08:28 +0600

    Setup project with Spring Initializr

---
+++ src/main/java/com/alatoo/socialEventManagement/SocialEventManagementApplication.java
@@ -0,0 +1,13 @@
+package com.alatoo.socialEventManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialEventManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialEventManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alatoo/socialEventManagement/SocialEventManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alatoo.socialEventManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialEventManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
